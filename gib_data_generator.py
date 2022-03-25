eaed5705f1d7bada3c2acba26236217018608fc8
yunifuchioka <fuchioka@cs.ubc.ca>
2022-03-24 19:37:45 -0700

    preload dataset in memory in data generator

--- gib_data_generator.py
+++ gib_data_generator.py
@@ -27,6 +27,39 @@ class GibGenerator(BaseGenerator):
             np.full(self.compute_keypoints_shape(), -1).flatten().astype(np.int16)
         )
 
+        # load entire gibbon dataset onto memory
+        images_array = []
+        keypoints_array = []
+        for index in range(self.num_data):
+            curr = self.df_gib.loc[index]
+            curr_img = np.array(Image.open("dataset/train/" + curr["file"]))
+
+            # determine image size and pad accordingly
+            img_shape = self.compute_image_shape()
+            curr_img_height, curr_img_width, _ = curr_img.shape
+            top_pad = np.floor((img_shape[0] - curr_img_height) / 2).astype(np.uint16)
+            bottom_pad = np.ceil((img_shape[0] - curr_img_height) / 2).astype(np.uint16)
+            right_pad = np.ceil((img_shape[1] - curr_img_width) / 2).astype(np.uint16)
+            left_pad = np.floor((img_shape[1] - curr_img_width) / 2).astype(np.uint16)
+            curr_img = np.pad(
+                curr_img,
+                ((top_pad, bottom_pad), (left_pad, right_pad), (0, 0)),
+                mode="constant",
+                constant_values=0,
+            )
+
+            curr_pose_array = np.array(curr["landmarks"]).reshape(
+                self.compute_keypoints_shape()
+            )
+            curr_pose_array[:, 0] += left_pad
+            curr_pose_array[:, 1] += top_pad
+
+            images_array.append(curr_img)
+            keypoints_array.append(curr_pose_array)
+        self.images = np.stack(images_array)
+        self.keypoints = np.stack(keypoints_array).astype(np.float64)
+
+
     def __len__(self):
         """
         Returns the number of samples in the generator as an integer (int64)
@@ -57,28 +90,7 @@ class GibGenerator(BaseGenerator):
         Returns a numpy array of images with the shape:
         (n_samples, height, width, n_channels)
         """
-        images_array = []
-        for index in indexes:
-            curr = self.df_gib.loc[index]
-            curr_img = np.array(Image.open("dataset/train/" + curr["file"]))
-
-            # determine image size and pad accordingly
-            img_shape = self.compute_image_shape()
-            curr_img_height, curr_img_width, _ = curr_img.shape
-            top_pad = np.floor((img_shape[0] - curr_img_height) / 2).astype(np.uint16)
-            bottom_pad = np.ceil((img_shape[0] - curr_img_height) / 2).astype(np.uint16)
-            right_pad = np.ceil((img_shape[1] - curr_img_width) / 2).astype(np.uint16)
-            left_pad = np.floor((img_shape[1] - curr_img_width) / 2).astype(np.uint16)
-            curr_img = np.pad(
-                curr_img,
-                ((top_pad, bottom_pad), (left_pad, right_pad), (0, 0)),
-                mode="constant",
-                constant_values=0,
-            )
-
-            images_array.append(curr_img)
-        images = np.stack(images_array)
-        return images
+        return self.images[indexes,:,:,:]
 
     def get_keypoints(self, indexes):
         """
@@ -87,24 +99,7 @@ class GibGenerator(BaseGenerator):
         Returns a numpy array of keypoints with the shape:
         (n_samples, n_keypoints, 2), where 2 is the x,y coordinates
         """
-        keypoints_array = []
-        for index in indexes:
-            curr = self.df_gib.loc[index]
-            curr_pose_array = np.array(curr["landmarks"]).reshape(
-                self.compute_keypoints_shape()
-            )
-
-            curr_img = np.array(Image.open("dataset/train/" + curr["file"]))
-            curr_img_height, curr_img_width, _ = curr_img.shape
-            img_shape = self.compute_image_shape()
-            top_pad = np.floor((img_shape[0] - curr_img_height) / 2).astype(np.uint16)
-            left_pad = np.floor((img_shape[1] - curr_img_width) / 2).astype(np.uint16)
-            curr_pose_array[:, 0] += left_pad
-            curr_pose_array[:, 1] += top_pad
-
-            keypoints_array.append(curr_pose_array)
-        keypoints = np.stack(keypoints_array).astype(np.float64)
-        return keypoints
+        return self.keypoints[indexes,:,:]
 
     def set_keypoints(self, indexes, keypoints):
         """
