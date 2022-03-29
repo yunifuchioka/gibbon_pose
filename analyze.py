257dee71b32ea92f4d5378eff4b83f6916bcbf9d
yunifuchioka <fuchioka@cs.ubc.ca>
2022-03-28 19:02:20 -0700

    added support for evaluating prediction on videos

--- analyze.py
+++ analyze.py
@@ -7,9 +7,7 @@ from deepposekit.io import VideoReader
 from gib_data_generator import GibGenerator
 from utils import *
 
-if __name__ == "__main__":
-    model = load_model("results/dlc_40.h5")
-
+def analyze_train_set(model):
     generator = GibGenerator()
 
     indices = np.random.randint(low=0, high=len(generator), size=10)
@@ -20,3 +18,38 @@ if __name__ == "__main__":
         plot_img(img[0, :, :, :], keypoint_array2dict(pose))
         plt.title(idx)
         plt.show()
+
+def analyze_video(model):
+    generator = VideoReader("videos/brach-2D-crop.mp4", batch_size=1)
+    fig = plt.figure()
+
+    def draw_frame(k):
+        fig.clear()
+        img = generator[k]
+        predictions = model.predict_on_batch(img)
+        pose = predictions[0, :, :2]
+        plot_img(img[0, :, :, :], keypoint_array2dict(pose))
+
+    import matplotlib.animation as animation
+
+    anim = animation.FuncAnimation(
+        fig,
+        draw_frame,
+        frames=len(generator) // 2,
+        interval= 0.033 * 1000.0,
+        repeat=True,
+        blit=False,
+    )
+
+    Writer = animation.writers["ffmpeg"]
+    writer = Writer(fps=33.3, metadata=dict(artist="Me"), bitrate=1000)
+    anim.save("videos/brach-2D-pred.mp4", writer=writer)
+
+    plt.show()
+
+if __name__ == "__main__":
+    model = load_model("results/dlc_40.h5")
+    # model = load_model("results/03-24-2022_23-44-25.h5")
+
+    # analyze_train_set(model)
+    analyze_video(model)
---
+++ video_img_utils.py
@@ -0,0 +1,62 @@
+import cv2
+
+import numpy as np
+import cv2
+
+# video cropping code taken from
+# https://stackoverflow.com/a/61724147
+
+# Open the video
+cap = cv2.VideoCapture("videos/brach-2D.mp4")
+
+# Initialize frame counter
+cnt = 0
+
+# Some characteristics from the original video
+w_frame, h_frame = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(
+    cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+)
+fps, frames = cap.get(cv2.CAP_PROP_FPS), cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+# Here you can define your croping values
+w = 512
+h = 512
+x = w_frame // 2 - w // 2
+y = h_frame // 2 - h // 2 - 100
+
+# output
+out = cv2.VideoWriter(
+    "videos/brach-2D-crop.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
+)
+
+
+# Now we start
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    cnt += 1  # Counting frames
+
+    # Avoid problems when video finish
+    if ret == True:
+        # Croping the frame
+        crop_frame = frame[y : y + h, x : x + w]
+
+        # Percentage
+        xx = cnt * 100 / frames
+        print(int(xx), "%")
+
+        out.write(crop_frame)
+
+        # Just to see the video in real time
+        cv2.imshow("frame", frame)
+        cv2.imshow("croped", crop_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
